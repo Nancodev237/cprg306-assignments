183856d005044bb5063f5c7442dcd58cffb0ec8f
Nancodev237 <yongalanapayos@gmail.com>
2026-01-22 22:16:30 -0700

    I have completed the home page and student info page

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Alan Ngayo Nankam</p>
+      <p>Github:
+        <Link href=""> cprg306-assignments</Link>
+      </p>
+      
+    </div>
+  )
+}
+
+export default StudentInfo
